035b8ca3e1679a815c2c7d03be3060a7cfd77c63
Diego A Villalpando V <dialvive@protonmail.com>
2021-09-07 19:29:19 -0500

    Add other excel format

--- src/file/fileExtension.go
+++ src/file/fileExtension.go
@@ -5,7 +5,8 @@ type FileExtension string
 const (
 	Txt     FileExtension = ".txt"
 	Csv     FileExtension = ".csv"
-	Excel   FileExtension = ".xlsx"
+	Excel1  FileExtension = ".xls"
+	Excel2  FileExtension = ".xlsx"
 	Json    FileExtension = ".json"
 	Graphql FileExtension = ".graphql"
 	Sql     FileExtension = ".sql"
